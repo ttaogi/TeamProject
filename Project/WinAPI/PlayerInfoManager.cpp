21130412f6e89d53edb913ce00ae724cfb485807
DESKTOP-93KRPCL\BN927A <qkrwoqud431@naver.com>
2022-03-01 22:39:59 +0900

    Merge.

--- Project/WinAPI/PlayerInfoManager.cpp
+++ Project/WinAPI/PlayerInfoManager.cpp
@@ -33,55 +33,48 @@ void PlayerInfoManager::setMoney(int _money) { money = _money; }
 Item PlayerInfoManager::getShovel() const { return shovel; }
 void PlayerInfoManager::setShovel(Item _shovel)
 {
-	if (_shovel.type == ITEM_TYPE::SHOVEL ||
-		_shovel.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_shovel.type == ITEM_TYPE::SHOVEL || _shovel.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		shovel = _shovel;
 }
 
 Item PlayerInfoManager::getAttack() const { return attack; }
 void PlayerInfoManager::setAttack(Item _attack)
 {
-	if (_attack.type == ITEM_TYPE::ATTACK ||
-		_attack.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_attack.type == ITEM_TYPE::ATTACK || _attack.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		attack = _attack;
 }
 
 Item PlayerInfoManager::getBody() const { return body; }
 void PlayerInfoManager::setBody(Item _body)
 {
-	if (_body.type == ITEM_TYPE::BODY ||
-		_body.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_body.type == ITEM_TYPE::BODY || _body.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		body = _body;
 }
 
 Item PlayerInfoManager::getHead() const { return head; }
 void PlayerInfoManager::setHead(Item _head)
 {
-	if (_head.type == ITEM_TYPE::HEAD ||
-		_head.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_head.type == ITEM_TYPE::HEAD || _head.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		head = _head;
 }
 
 Item PlayerInfoManager::getTorch() const { return torch; }
 void PlayerInfoManager::setTorch(Item _torch)
 {
-	if (_torch.type == ITEM_TYPE::TORCH ||
-		_torch.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_torch.type == ITEM_TYPE::TORCH || _torch.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		torch = _torch;
 }
 
 Item PlayerInfoManager::getHeal() const { return heal; }
 void PlayerInfoManager::setHeal(Item _heal)
 {
-	if (_heal.type == ITEM_TYPE::HEAL ||
-		_heal.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_heal.type == ITEM_TYPE::HEAL || _heal.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		heal = _heal;
 }
 
 Item PlayerInfoManager::getBomb() const { return bomb; }
 void PlayerInfoManager::setBomb(Item _bomb)
 {
-	if (_bomb.type == ITEM_TYPE::BOMB ||
-		_bomb.type == ITEM_TYPE::ITEM_TYPE_NUM)
+	if (_bomb.type == ITEM_TYPE::BOMB || _bomb.type == ITEM_TYPE::ITEM_TYPE_NUM)
 		bomb = _bomb;
 }
